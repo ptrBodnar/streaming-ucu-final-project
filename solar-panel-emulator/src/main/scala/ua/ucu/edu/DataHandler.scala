cfcccf053578e10da418f12d5d70281fc23109a9
ptr_bdr <peterbodnar1213@gmail.com>
2020-03-07 22:28:15 +0200

    finilizing work for today

--- solar-panel-emulator/src/main/scala/ua/ucu/edu/DataHandler.scala
+++ solar-panel-emulator/src/main/scala/ua/ucu/edu/DataHandler.scala
@@ -11,7 +11,6 @@ class DataHandler {
   case class CodesCPV(code: String, description: String, risk: Int)
 
   object CodesCPV {
-
     implicit val reads: Reads[CodesCPV] = (
       (JsPath \ "Код CPV").read[String] ~
         (JsPath \ "Опис").read[String] ~
@@ -23,7 +22,7 @@ class DataHandler {
     val json: BufferedSource = scala.io.Source.fromResource("cpvRisk.json")
     val json_str: String = try json.mkString finally json.close()
 
-    var CPVarray = Json.parse(json_str).as[List[CodesCPV]]
+    val CPVarray = Json.parse(json_str).as[List[CodesCPV]]
 
     CPVarray
   }
--- solar-panel-emulator/src/main/scala/ua/ucu/edu/Main.scala
+++ solar-panel-emulator/src/main/scala/ua/ucu/edu/Main.scala
@@ -1,24 +1,14 @@
 package ua.ucu.edu
-
-import ua.ucu.edu.kafka.DummyDataProducer
-
-import scala.io.BufferedSource
-import play.api.libs.json._
-
-import ua.ucu.edu.DataHandler
-
+so
 
 object Main extends App {
 
 
   val handler = new DataHandler()
-
   val a = handler.parseJSON()
 
   println(a.map(_.description))
 
-
-//  println(tweets_array(0).get("Код CPV").get)
 
 //  DummyDataProducer.pushTestData()
 }
