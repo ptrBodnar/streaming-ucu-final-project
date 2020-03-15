fc71a2cd4991bc57fee9770ff58483435a9d16ec
Olena Shevchenko <olenashevch@gmail.com>
2020-03-15 17:47:17 +0200

    remove braskets in kafka config

--- kafka_client.sh
+++ kafka_client.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-ssh -i "kafka-client-ec2.pem " ec2-user@ec2-54-162-46-116.compute-1.amazonaws.com
+ssh -i kafka-client-ec2.pem ec2-user@ec2-54-162-46-116.compute-1.amazonaws.com
